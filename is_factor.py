0a01e0311894feec8d4576b8b6aa515bc23cd9bb
cyunier <consuegra.yunier@rutgers.edu>
2016-04-10 16:40:03 -0400

    Demo

--- is_factor.py
+++ is_factor.py
@@ -1,5 +1,18 @@
-def is_factor(a, b):
-	if b % a == 0:
-		return True
-	else:
-		return False
+'''
+Find the factos of integer
+'''
+
+def factors(b):
+	for i in range(1, b+1):
+		if b % i == 0:
+			print(i)
+
+if __name__ == '__main__':
+
+	b = input('Enter a number: ')
+	b = float(b)
+
+    if (b > 0) and b.is_integer():
+    	factors(int(b))
+    else:
+    	print('Enter a positive number')
